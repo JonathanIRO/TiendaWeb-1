98dbed8a35f0d166d5d31bfbc0d360b957e39d73
JonathanIRO <64859405+JonathanIRO@users.noreply.github.com>
2021-09-22 14:27:51 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
     $servidor="localhost";
-    $nombreBD="tienda";
-    $usuario="isaac";
-    $pass="admin1234";
+    $nombreBD="store";
+    $usuario="root";
+    $pass=" ";
     $conexion = new mysqli($servidor,$usuario,$pass,$nombreBD);
     if($conexion -> connect_error){
         die("No se pudo conectar");
