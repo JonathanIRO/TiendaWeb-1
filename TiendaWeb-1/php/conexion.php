6bad90ec6d5a2053a1926c6743977fc56f975c92
JonathanIRO <jonaisaacro@gmail.com>
2021-09-22 13:53:52 -0500

    u

--- TiendaWeb-1/php/conexion.php
+++ TiendaWeb-1/php/conexion.php
@@ -1,8 +1,8 @@
 <?php
     $servidor="localhost";
     $nombreBD="tienda";
-    $usuario="root";
-    $pass="";
+    $usuario="isaac";
+    $pass="admin1234";
     $conexion = new mysqli($servidor,$usuario,$pass,$nombreBD);
     if($conexion -> connect_error){
         die("No se pudo conectar");
